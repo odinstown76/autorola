20a392103316f0b5e639a9fdfeee1598deb5f54c
Morten Knudsen <morten.knudsen@quickorder.io>
2024-09-20 07:50:13 +0200

    feat: laravel framework setup

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Domain\Model\CarRepository;
+use Domain\SimpleUuidFactory;
+use Domain\SimpleUuidInterFace;
+use Illuminate\Support\ServiceProvider;
+use Infrastructure\Repositories\CarRepositoryUsesInMemory;
+use Infrastructure\Support\SimpleUuidFactoryUsingRamsey;
+use Infrastructure\Support\SimpleUuidUsingRamsey;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+}
